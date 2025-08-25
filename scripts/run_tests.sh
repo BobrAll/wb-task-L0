a16f4df7df1719b619f72e49212241f234d3121e
bobr <bobrus91@gmail.com>
2025-08-26 00:10:18 +0300

    fixed tests running

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Running unit tests..."
-go test test/unit/ -v
+go test test/unit/* -v
 
 echo "Running integration tests..."
-go test test/integration/ -v -timeout 30s
+go test test/integration/* -v -timeout 30s
